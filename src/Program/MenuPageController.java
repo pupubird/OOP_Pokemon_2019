9f72b2a74127bcec89a207df861ef37331f27938
pupubird <rainchai4240@gmail.com>
2019-11-05 23:37:30 +0800

    added background image

--- src/Program/MenuPageController.java
+++ src/Program/MenuPageController.java
@@ -2,8 +2,12 @@ package Program;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class MenuPageController {
     @FXML
@@ -27,7 +31,7 @@ public class MenuPageController {
     public ImageView exitButtonImage;
 
     @FXML
-    public void initialize(){
+    public void initialize() throws FileNotFoundException {
         int widthRatio = 10;
         int heightRatio = 24;
         int imageWidthRatio = 5;
@@ -40,6 +44,12 @@ public class MenuPageController {
         MenuPagePane.setMinHeight(height);
         MenuPagePane.setMaxWidth(width);
         MenuPagePane.setMaxHeight(height);
+
+        MenuPagePane.setStyle(
+                "-fx-background-image:url("+getClass().getResource("resources/fxml/assets/background.jpg")+");" +
+                        "-fx-background-repeat:no-repeat;" +
+                        "-fx-background-size:cover;"
+        );
 
         menuLogo.setFitWidth(width/imageWidthRatio);
         menuLogo.setFitHeight(height/imageHeightRatio);
