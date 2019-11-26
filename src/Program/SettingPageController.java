d99e5139fe37c17681bfe5ccd734f18c5d00c9e3
Dingnh <18039503@imail.sunway.edu.my>
2019-11-26 15:39:56 +0800

    SettingPage Volume Control done

--- src/Program/SettingPageController.java
+++ src/Program/SettingPageController.java
@@ -2,6 +2,7 @@ package Program;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
@@ -9,13 +10,51 @@ import java.net.URL;
 
 
 public class SettingPageController {
+    private double volume = 0.5;
+
     @FXML
     public VBox SettingPagePane;
     public ImageView menuLogo;
+    public Button volumeDownButton;
+    public Button volumeUpButton;
+    public Label volumeValueLabel;
     public ImageView returnButtonImage;
     public Button returnButton;
 
     private URL clickingEffect = getClass().getResource("resources/fxml/assets/mouseClick.mp3");
+
+    public void volumeDown() {
+        if (volume > 0.01) {
+
+            volume -= 0.05;
+
+            volumeValueLabel.setText(
+                    translateVolume(volume)
+            );
+            ControllerUtil.audioplayer.setVolume(volume);
+
+        }
+    }
+
+    public void volumeUp() {
+        if (volume < 1) {
+
+            volume += 0.05;
+
+            volumeValueLabel.setText(
+                    translateVolume(volume)
+            );
+            ControllerUtil.audioplayer.setVolume(volume);
+
+        }
+    }
+
+    public String translateVolume(double vol) {
+        // return volume in percentage format (45%)
+        String volumePercentage = ((int) Math.round(vol * 100)) + "%";
+
+        return volumePercentage;
+    }
 
     public void navigateToMenuPage(){
         ControllerUtil.playEffect(clickingEffect);
